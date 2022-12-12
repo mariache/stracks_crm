a33bcd68df3693f5ed1348ede8425a16d5c69742
salam.omarbagaev <salam.omarbagaev@bpsoftware.net>
2022-12-13 11:10:05 +1300

    - eslint prettier setup

---
+++ .eslintrc.cjs
@@ -0,0 +1,41 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended'
+  ],
+  overrides: [
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+    "prettier"
+  ],
+  rules: {
+    "react/react-in-jsx-scope": 0,
+    "@typescript-eslint/quotes": 0,
+    "react/function-component-definition": 0,
+    "import/prefer-default-export": 0,
+    "react/require-default-props" :0,
+    "no-restricted-globals": 0,
+    'prettier/prettier': [
+      'error',
+      {
+        'endOfLine': 'auto',
+      }
+    ]
+  },
+};
---
+++ .prettierrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+    trailingComma: "es5",
+    tabWidth: 2,
+    semi: true,
+    singleQuote: false,
+    endOfLine: "lf",
+    trailingComma: "none",
+    arrowParens: "avoid",
+    importOrder: ["^[a-z]", "^[@]", "^[.]"],
+    importOrderCaseInsensitive: true,
+    importOrderSeparation: true,
+    importOrderSortSpecifiers: true,
+  };
